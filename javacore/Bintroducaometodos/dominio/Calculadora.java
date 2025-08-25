b6d686a86cc46ba562b5b4e8a6e330bcd3966671
Augusto Cézar <augustoccfaria@gmail.com>
2025-08-25 20:39:44 -0300

    arrumando os packages(eu acho)

--- javacore/Bintroducaometodos/dominio/Calculadora.java
+++ javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -1,4 +1,4 @@
-package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+package javacore.Bintroducaometodos.dominio;
 
 public class Calculadora {
 
@@ -16,6 +16,38 @@ public class Calculadora {
 
 
     public double divideDoisNumeros(double num1, double num2){
+        if (num2 == 0) {
+            return 0;
+        }
         return num1/num2;
     }
+
+    public double divideDoisNumeros02(double num1, double num2){
+        if (num2 != 0) {
+            return num1/num2;
+        }else{
+
+        System.out.println("Nao existe divisao por zero");
+        return 0;
+        }
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2){
+        if (num2 == 0) {
+
+            System.out.println("Nao existe divisao por zero");
+            return;
+        }
+        System.out.println(num1/num2);
+        
+    }
+
+    public void alteraDoisNumeros(int num1, int num2){
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteradoDoisNumeros");
+        System.out.println("Num1 "+num1);
+        System.out.println("Num2 "+num2);
+    }
+
 }
---
+++ javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -0,0 +1,24 @@
+package javacore.Bintroducaometodos.test;
+
+import javacore.Bintroducaometodos.dominio.Estudante;
+import javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante1 = new Estudante();
+        Estudante estudante2 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante1.nome = "Augusto";
+        estudante1.idade = 19;
+        estudante1.sexo = 'M';
+
+        estudante2.nome = "Ana";
+        estudante2.idade = 20;
+        estudante2.sexo = 'F';
+
+        impressora.imprime(estudante1);
+        
+        impressora.imprime(estudante2);
+    }
+}
